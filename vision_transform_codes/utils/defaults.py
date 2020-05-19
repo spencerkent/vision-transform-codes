f33b7784c3e90844a37fe3273751af1087522ce0
Spencer Kent <spencer.kent@berkeley.edu>
2020-05-19 13:46:58 -0700

    move gabor fitting to other library (see github.com/spencerkent/gabor-fitting)

--- vision_transform_codes/utils/defaults.py
+++ vision_transform_codes/utils/defaults.py
@@ -14,6 +14,6 @@ raw_data_filepaths = {
     'Kodak_BW': dataset_directory / 'Kodak/kodak_full_images_training.p'}
 
 # just useful for sampling a gabor without having to specify all the parameters
-gabor_params = {'filter_size': (16, 16), 'filter_parameters': {
+gabor_params = {'patch_size': (16, 16), 'gabor_parameters': {
   'orientation': np.pi/4, 'envelope_width': 3, 'envelope_aspect': 0.5,
   'frequency': 1/4, 'phase': 0, 'position_yx': (0, 0)}}
